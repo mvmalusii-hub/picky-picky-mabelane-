00bcf92ba0ec80c472d340bfe785bc63fdc4e685
Msimelelo Malusi <mv.malusii@gmail.com>
2026-06-04 15:11:09 +0200

    Create  knexfile.js

---
+++ backend/knexfile.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: { directory: './src/migrations' },
+    seeds: { directory: './src/seeds' },
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: { directory: './src/migrations' },
+    seeds: { directory: './src/seeds' },
+  },
+};
