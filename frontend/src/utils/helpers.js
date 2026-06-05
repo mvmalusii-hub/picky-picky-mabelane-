56f3d03e713bd7b80097f1d8380e2e6286306266
Msimelelo Malusi <mv.malusii@gmail.com>
2026-06-05 15:51:55 +0200

    Update helpers.js

--- frontend/src/utils/helpers.js
+++ frontend/src/utils/helpers.js
@@ -1,1 +1,9 @@
+export const formatCurrency = (amount) => `R${amount}`;
 
+export const truncate = (str, maxLen = 50) => str.length > maxLen ? str.slice(0, maxLen) + '…' : str;
+
+export const getInitials = (name) => name.split(' ').map(n => n[0]).join('').toUpperCase();
+
+export const copyToClipboard = (text) => navigator.clipboard.writeText(text);
+
+export const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
